67e014e40297424f216586784fff7f4397ae52d0
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-15 23:37:36 +0530

    Added: UC1

--- Employee.java
+++ Employee.java
@@ -1,5 +1,19 @@
-public class Employee {
-public static void main(String[] args){
-System.out.println("Welcome to Employee Problem");
-}
+public class EmployeeWage{
+	public static void main(String[] args){
+		
+		int isFullTime=1;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present");
+			
+		}
+		else
+		{
+			System.out.println("Employee is Abscent");
+		}
+		
+	}
+	
 }
