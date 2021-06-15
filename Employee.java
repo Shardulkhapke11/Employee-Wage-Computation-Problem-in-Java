2a1a3054b14638d3c3724e63dd9e25972b41fd7f
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-15 23:54:55 +0530

    Added: UC2

--- Employee.java
+++ Employee.java
@@ -1,12 +1,16 @@
-public class EmployeeWage{
+public class Employee{
 	public static void main(String[] args){
 		
 		int isFullTime=1;
+		int WagePerHr=20;
+		int FullDayHr=8;
 		
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
+			int DailyEmpWage=WagePerHr*FullDayHr;
+			System.out.println("DailyEmpWage::"+DailyEmpWage);
 			
 		}
 		else
