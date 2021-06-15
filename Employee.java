c4935c5aa9a616f9736b00f754ce20dfa74462b6
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-16 00:10:22 +0530

    Added: UC3

--- Employee.java
+++ Employee.java
@@ -2,22 +2,28 @@ public class Employee{
 	public static void main(String[] args){
 		
 		int isFullTime=1;
+		int isPartTime=2;
 		int WagePerHr=20;
 		int FullDayHr=8;
+		int empHr;
 		
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random()*10)%3;
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
-			int DailyEmpWage=WagePerHr*FullDayHr;
-			System.out.println("DailyEmpWage::"+DailyEmpWage);
+		empHr=8;
+			
 			
 		}
-		else
+		else if(empCheck == isPartTime)
 		{
 			System.out.println("Employee is Abscent");
+			empHr=4;
+		}else
+		{
+			empHr=0;
+			
 		}
 		
-	}
-	
-}
+		int DailyEmpWage=WagePerHr*empHr;
+		Syst
